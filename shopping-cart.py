8ec1b730f33f755eb7ab27a520fee91c1076476a
João Cardoso <133004890+srv-joaocardoso@users.noreply.github.com>
2026-01-07 22:45:41 -0300

    Enhance add method with quantity support
    Updated the add method to include quantity and modified total calculation to account for item quantities.

--- shopping-cart.py
+++ shopping-cart.py
@@ -10,21 +10,21 @@ class ShoppingCart:
   def __len__(self):
     return len(self.itens)
 
-  def add(self, item):
-    self.itens.append(item)
+  def add(self, item, qtt=1):
+    if isinstance(qtt, (int, float)):
+      self.itens.append({"qtt": qtt, "item": item})
 
   def show(self):
     return self.itens
 
   def remove(self, index):
-    self.pop(index)
+    self.itens.pop(index)
 
   def total(self):
-    sum = 0
-    for item in self.itens:
-      sum+= item.price
-
-    return sum
+    total = 0
+    for element in self.itens:
+      total+= element["item"].price * element["qtt"]
+    return total
     
 class User:
   def __init__(self, first_name, second_name):
